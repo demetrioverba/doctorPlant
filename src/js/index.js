2ea64c906525cb92d53e564a5f62141107f1aaab
demetrioverba <65371371+demetrioverba@users.noreply.github.com>
2020-09-19 19:15:04 +0300

    Building the Search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-  const proxy = 'https://cors-anywhere.herokuapp.com/'
-  const key = '-ZZ_xWYS4b9bI1ZtT7emUqd1Mm1qmxYNuR7sjJQsI_A';
-  const response = await axios(`${proxy}https://trefle.io/api/v1/plants/search?token=${key}&q=${query}`);
-  console.log(response);
-
-}
-getResults('yucca');
-
-
-
-
-
-
-
-
+const search = new Search('coconut');
+console.log(search);
+search.getResults();
