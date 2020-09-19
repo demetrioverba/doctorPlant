75c506747f056f0f1c2671bf2ae8d9d3d3f81368
demetrioverba <65371371+demetrioverba@users.noreply.github.com>
2020-09-19 16:10:44 +0300

    Making first query

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,18 @@
-import num from './test';
+import axios from 'axios';
 
-const testing = 'blah blah';
-console.log(testing);
+async function getResults(query) {
+  const proxy = 'https://cors-anywhere.herokuapp.com/'
+  const key = '-ZZ_xWYS4b9bI1ZtT7emUqd1Mm1qmxYNuR7sjJQsI_A';
+  const response = await axios(`${proxy}https://trefle.io/api/v1/plants/search?token=${key}&q=${query}`);
+  console.log(response);
 
-console.log(`I imported ${num} from another module`)
+}
+getResults('yucca');
+
+
+
+
+
+
+
+
